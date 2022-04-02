acbc1de9bb3ba472471aab316e3b832a5268bad8
Oguzhan <oguzhnatly@gmail.com>
2022-04-02 23:54:55 +0300

    build folder added.

---
+++ build/index.d.ts
@@ -0,0 +1,8 @@
+declare const RNImageManipulator: any;
+export declare type RNImageManipulatorResult = {
+    uri: string;
+    width: number;
+    height: number;
+    base64?: string;
+};
+export default RNImageManipulator;
